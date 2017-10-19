bb4add91cb01b6a786c11746577899894b23a69a
Justin Hunt <poodllsupport@gmail.com>
2017-10-19 05:38:08 +0000

    Initial commit of ishineemail signup

---
+++ classes/helper.php
@@ -0,0 +1,94 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace auth_ishineemail;
+
+defined('MOODLE_INTERNAL') || die();
+
+
+/**
+ *
+ * This is a class containing static functions for general PoodLL filter things
+ * like embedding recorders and managing them
+ *
+ * @package   auth_ishineemail
+ * @since      Moodle 3.2
+ * @copyright  2017 Justin Hunt
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+ 
+class helper
+{
+
+	/**
+     * Adds code snippet to a moodle form object for custom profile fields that
+     * should appear on the signup page
+     * @param moodleform $mform moodle form object
+     */
+    public static function fetch_prefecture_list() {
+        $prefs = array('北海道',
+'青森県',
+'岩手県',
+'宮城県',
+'秋田県',
+'山形県',
+'福島県',
+'茨城県',
+'栃木県',
+'群馬県',
+'埼玉県',
+'千葉県',
+'東京都',
+'神奈川県',
+'新潟県',
+'富山県',
+'石川県',
+'福井県',
+'山梨県',
+'長野県',
+'岐阜県',
+'静岡県',
+'愛知県',
+'三重県',
+'滋賀県',
+'京都府',
+'大阪府',
+'兵庫県',
+'奈良県',
+'和歌山県',
+'鳥取県',
+'島根県',
+'岡山県',
+'広島県',
+'山口県',
+'徳島県',
+'香川県',
+'愛媛県',
+'高知県',
+'福岡県',
+'佐賀県',
+'長崎県',
+'熊本県',
+'大分県',
+'宮崎県',
+'鹿児島県',
+'沖縄県');
+
+return $prefs;
+    }
+
+
+}//end of class
